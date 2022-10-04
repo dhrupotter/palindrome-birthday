9e3dcf515e967eca5b7e87b20d0b2036d2a64442
dhrupotter <dhruvigandhi25@gmail.com>
2022-10-04 16:00:02 +0530

    JS logic implemented

---
+++ app.js
@@ -0,0 +1,7 @@
+let inputDate = document.querySelector("#inputDate");
+let form = document.querySelector("#form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("clicked");
+});
